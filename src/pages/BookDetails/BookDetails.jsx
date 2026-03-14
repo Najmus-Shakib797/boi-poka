9b2f9df2664e5c2025d7cfdcc88018d36a6c48bd
najmus shakib <nshakib797@gmail.com>
2026-03-14 20:22:41 +0600

    5th-commit

--- src/pages/BookDetails/BookDetails.jsx
+++ src/pages/BookDetails/BookDetails.jsx
@@ -1,12 +1,35 @@
 import React from "react";
-import { useParams } from "react-router";
+import { useLoaderData, useParams } from "react-router";
+import { addToStoredDB } from "../../utility/AddToDB";
 
 const BookDetails = () => {
   const { id } = useParams();
-  console.log(id);
+  //   console.log(id);
+  const data = useLoaderData();
+  //   console.log(data);
+  const bookId = parseInt(id);
+  const singleBook = data.find((book) => book.bookId === bookId);
+  //   console.log(singleBook);
+  const { bookName, image, author } = singleBook;
+
+  const handleMarkAsRead = (id) => {
+    addToStoredDB(id);
+  };
   return (
-    <div>
-      <h1>Ami book details</h1>
+    <div className="w-2/3 mx-auto">
+      <div>
+        <img className="w-45" src={image} alt="" />
+        <h5>
+          {bookName} : <br /> <small> {author}</small>
+        </h5>
+        <button
+          onClick={() => handleMarkAsRead(id)}
+          className="btn btn-accent m-2"
+        >
+          Mark as Read
+        </button>
+        <button className="btn btn-info m-2">Add to WshList</button>
+      </div>
     </div>
   );
 };
